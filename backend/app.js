56e655e83ce2a9238ae746df3a1ce56aca558621
yugani <yugani@outlook.fr>
2019-12-10 19:15:33 +0100

    back-end avec controleurs, modeles et routes

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,21 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require('mongoose');
+const config = 'mongodb://localhost:27017/DashboardProject';
+//let cors = require(cors);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).then(() => 
+{
+    console.log('Database connected successfully !')
+},
+)
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var sensorRouteur = require("./routes/sensors");
+var measureRouteur = require("./routes/measures");
 
 var app = express();
 
@@ -15,5 +27,7 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/measure", measureRouteur);
+app.use("/sensor", sensorRouteur);
 
 module.exports = app;
